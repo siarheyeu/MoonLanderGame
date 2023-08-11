96d267d84cabc412c16dbc7421eec83686080791
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-08-11 23:59:52 +0300

    Correction of the error of "leaving" the rocket off the screen

--- Rocket.java
+++ Rocket.java
@@ -1,6 +1,7 @@
 package com.javarush.games.moonlander;
 
 public class Rocket extends GameObject {
+    private double speedY = 0;
     public Rocket (double x, double y) {
         super(x, y, ShapeMatrix.ROCKET);
     }
